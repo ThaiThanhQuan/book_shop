93e4de20d15d6a6521b41ca99fbce939ce674cb5
Quan Thai <thaithanhquan11102005@gmail.com>
2025-11-13 22:10:36 +0700

    set up router

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 function Layout() {
     return (
         <div>
-            Hello world with quanthai
+            <AppHeader />
+            <Outlet />
         </div>
     )
 }
